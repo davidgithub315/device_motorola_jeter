7ae772f45fd773f801257eda335f39a07d771433
TogoFire <italomellopereira@gmail.com>
2022-10-29 10:29:49 -0300

    jeter: rootdir: drop swappiness
    This is defined in the kernel. We don't need it.
    
    Change-Id: 4d8958f508ff0399751ca1d5cb5654deffa03ede
    Signed-off-by: TogoFire <italomellopereira@gmail.com>

--- rootdir/bin/init.qcom.post_boot.sh
+++ rootdir/bin/init.qcom.post_boot.sh
@@ -466,7 +466,6 @@ if [ "$ProductName" == "msmnile" ] || [ "$ProductName" == "kona" ] ; then
       configure_zram_parameters
       configure_read_ahead_kb_values
       echo 0 > /proc/sys/vm/page-cluster
-      echo 110 > /proc/sys/vm/swappiness
 else
     arch_type=`uname -m`
     MemTotalStr=`cat /proc/meminfo | grep MemTotal`
@@ -568,9 +567,6 @@ else
             echo 100 > /sys/module/vmpressure/parameters/allocstall_threshold
         fi
     fi
-
-    # Set swappiness to 100 for all targets
-    echo 110 > /proc/sys/vm/swappiness
 
     # Disable wsf for all targets beacause we are using efk.
     # wsf Range : 1..1000 So set to bare minimum value 1.
@@ -3859,7 +3855,6 @@ case "$target" in
 
             # Turn on sleep modes.
             echo 0 > /sys/module/lpm_levels/parameters/sleep_disabled
-            echo 100 > /proc/sys/vm/swappiness
             ;;
         esac
     ;;
@@ -4416,7 +4411,6 @@ case "$target" in
 	echo N > /sys/module/lpm_levels/L3/l3-dyn-ret/idle_enabled
         # Turn on sleep modes.
         echo 0 > /sys/module/lpm_levels/parameters/sleep_disabled
-	echo 100 > /proc/sys/vm/swappiness
 	echo 120 > /proc/sys/vm/watermark_scale_factor
     ;;
 esac
