796caac81608e5dc0c236f5f1d244188333ef248
SH4ND <miqueiasantos20@gmail.com>
2023-03-10 18:50:58 -0300

    jeter: Build MotoDoze

--- device.mk
+++ device.mk
@@ -82,6 +82,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/enginedefault/config/example/phone/audio_policy_engine_product_strategies.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_engine_product_strategies.xml \
     frameworks/av/services/audiopolicy/enginedefault/config/example/phone/audio_policy_engine_stream_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_engine_stream_volumes.xml
 
+# Doze
+PRODUCT_PACKAGES += \
+    MotoDoze
+
 # Camera
 PRODUCT_PACKAGES += \
     libstdc++.vendor \
