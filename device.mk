c29cb19a97ca532b950c595a8fefadec2c540af5
SH4ND <miqueiasantos20@gmail.com>
2023-04-02 13:24:44 -0300

    jeter: Remove some apps
    Signed-off-by: SH4ND <miqueiasantos20@gmail.com>

--- device.mk
+++ device.mk
@@ -39,14 +39,13 @@ PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
 
-# DEX
-PRODUCT_DEXPREOPT_SPEED_APPS += \
-    Settings \
-    SystemUI
-
 # APEX
 OVERRIDE_PRODUCT_COMPRESSED_APEX := false
 OVERRIDE_TARGET_FLATTEN_APEX := true
+
+# Remove unwanted packages
+PRODUCT_PACKAGES += \
+    debloater
 
 # Audio
 PRODUCT_PACKAGES += \
